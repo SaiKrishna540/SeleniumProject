73049b022aae120da3fb4e432c83eba9ac961a7e
SaiKrishna540 <krishnasai017@gmail.com>
2020-01-24 12:32:11 +0530

    Selenium Maven Automation Project

--- final-framework-testng/tests/com/training/regression/tests/LoginExcelTest.java
+++ final-framework-testng/tests/com/training/regression/tests/LoginExcelTest.java
@@ -8,11 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.training.bean.LoginBean;
-import com.training.dao.ELearningDAO;
 import com.training.dataproviders.LoginDataProviders;
 import com.training.generics.ScreenShot;
 import com.training.pom.LoginPOM;
@@ -51,7 +48,7 @@ public class LoginExcelTest {
 	@Test(dataProvider = "excel-inputs", dataProviderClass = LoginDataProviders.class)
 	public void loginDBTest(String userName, String password) {
 		loginPOM.sendUserName(userName);
-		loginPOM.sendPassword(password);
+		loginPOM.sendPassword();
 		loginPOM.clickLoginBtn();
 		screenShot.captureScreenShot(userName);
 
--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -4,13 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.training.generics.ScreenShot;
+
+//import com.training.generics.ScreenShot;
 import com.training.pom.LoginPOM;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
@@ -21,7 +23,7 @@ public class LoginTests {
 	private String baseUrl;
 	private LoginPOM loginPOM;
 	private static Properties properties;
-	private ScreenShot screenShot;
+//	private ScreenShot screenShot;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
@@ -35,21 +37,23 @@ public class LoginTests {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
 		loginPOM = new LoginPOM(driver); 
 		baseUrl = properties.getProperty("baseURL");
-		screenShot = new ScreenShot(driver); 
-		// open the browser 
+//		screenShot = new ScreenShot(driver); 
+//		open the browser 
 		driver.get(baseUrl);
 	}
 	
+	@Test
+	public void validLoginTest() {
+		loginPOM.sendUserName("SaiKrishna2");
+		loginPOM.sendPassword();
+		loginPOM.clickLoginBtn(); 
+//		screenShot.captureScreenShot("First");
+	}
+
 	@AfterMethod
 	public void tearDown() throws Exception {
 		Thread.sleep(1000);
-		driver.quit();
+//		driver.quit();
 	}
-	@Test
-	public void validLoginTest() {
-		loginPOM.sendUserName("admin");
-		loginPOM.sendPassword("admin@123");
-		loginPOM.clickLoginBtn(); 
-		screenShot.captureScreenShot("First");
-	}
+
 }
